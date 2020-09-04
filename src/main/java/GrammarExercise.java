5d7be3884dd358cac75a8a4bc88c9d3f9ba167cf
tanghaoran666 <18408230362@163.com>
2020-09-04 11:20:14 +0800

    [thr] feat: modify GrammarExerciseTest

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GrammarExercise {
+    private static Object Exception;
+    private static Object RuntimeException;
+
     public static void main(String[] args) {
         //需要从命令行读入
         String firstWordList = "";
@@ -13,6 +19,34 @@ public class GrammarExercise {
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
         //在这编写实现代码
-        return null;
+        List<String> first = new ArrayList<>(Arrays.asList(firstWordList.split(",")));
+        List<String> second = new ArrayList<>(Arrays.asList(secondWordList.split(",")));
+        ArrayList<String> firstAfterUpper = new ArrayList<>();
+        ArrayList<String> secondAfterUpper = new ArrayList<>();
+        firstAfterUpper = getUpperSpace(first);
+        secondAfterUpper = getUpperSpace(second);
+        firstAfterUpper.retainAll(secondAfterUpper);
+        List<String> expected = firstAfterUpper.stream().distinct().collect(Collectors.toList());
+        //List<String> expected sort
+        return expected;
+    }
+
+    private static ArrayList<String> getUpperSpace(List<String> first) {
+        ArrayList<String> result = new ArrayList<>();
+        for (String s : first) {
+            if ( s.length() == 0) throw new RuntimeException("input not valid");
+            s = s.toUpperCase();
+            String resultFirst = new String();
+            for(int i=0;i<s.length();i++){
+                char ch =s.charAt(i);
+                if(!Character.isUpperCase(ch)) throw new RuntimeException("input not valid");
+                if (i > 0) {
+                    resultFirst = resultFirst + " ";
+                }
+                resultFirst += ch;
+            }
+            result.add(resultFirst);
+        }
+        return  result;
     }
 }
